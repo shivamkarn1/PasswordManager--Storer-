13983b7d1f4fcd8a604dc83b303a2f8b52607f86
Shivam67Code <evilsniper64@gmail.com>
2025-09-27 07:08:25 +0545

    Updated Logo to black

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -42,9 +42,8 @@ function Navbar() {
           {/* Logo */}
           <div className="flex items-center space-x-3 cursor-pointer" onClick={() => navigate('/')}>
             <div className="w-10 h-10 bg-gradient-to-br from-slate-900 to-slate-700 dark:from-slate-100 dark:to-slate-300 rounded-xl flex items-center justify-center shadow-lg">
-              <svg className="w-6 h-6 text-white dark:text-slate-900" fill="currentColor" viewBox="0 0 20 20">
-                <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-              </svg>
+              {/* Replace the current logo src with the new black logo */}
+              <img src="/new black logo.png" alt="Logo" className="w-6 h-6" />
             </div>
             <div className="flex flex-col">
               <h1 className="text-xl font-bold text-slate-900 dark:text-slate-100 font-tagesschrift">
