b24c774a2904a9a43dad1fb44d8862982007f261
shivamkarn <shivamkarn767@gmail.com>
2025-11-12 08:08:48 +0545

    fixed the import statement

--- backend/test.js
+++ backend/test.js
@@ -1,6 +1,5 @@
 // PROGRAM TO GENERATE JWT TOKEN KEY (WHICH IS SIMPLY FOR JUST TESTING/PLACE-HOLDER)
 
-const crypto = require('crypto');
-
-const secret = crypto.randomBytes(64).toString('hex');
+import crypto from "crypto";
+const secret = crypto.randomBytes(128).toString("hex");
 console.log(secret);
